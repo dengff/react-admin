cd46c10de915d7555638a2d45efc1b4af290f2c3
dtf <dengtfwy@163.com>
2023-03-10 11:47:46 +0800

    fix

--- src/store/theme/reducers.js
+++ src/store/theme/reducers.js
@@ -19,6 +19,7 @@ export default function reducer(state = initTheme, action) {
       return {
         ...state,
         token: {
+          ...state.token,
           colorPrimary: action.colorPrimary,
         },
       };
